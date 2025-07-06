a2cf6fd9b4e95344f796cb79b8d52bca89124939
ferenk <fmoricz@gmail.com>
2025-07-06 15:35:38 +0200

    Demo: Simple calc() feature added

--- client/main.mjs
+++ client/main.mjs
@@ -1,6 +1,20 @@
 async function onPageLoaded()
 {
     document.title = 'Multi version client demo app';
+    document.getElementById('calculator').addEventListener('keydown', (event) => {
+        if (event.which == 13) {
+            const textarea = document.getElementById('calculator');
+            const inputExpr = textarea.value;
+
+            const resultMsg = `Calculation result: ${inputExpr} = ${ eval(inputExpr)}`;
+            const resEl = document.createElement('div');
+            resEl.innerHTML = resultMsg;
+            document.body.appendChild(resEl);
+
+            textarea.value = '';
+            alert(resultMsg);
+        }
+    } );
 }
 
 window.addEventListener("load", onPageLoaded);
