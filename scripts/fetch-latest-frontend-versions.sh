cc7fcbc0864f3629ee2a06985cf06b8f4621b748
ferenk <fmoricz@gmail.com>
2025-07-07 01:51:46 +0200

    Heroku client versions - from remote storage

---
+++ scripts/fetch-latest-frontend-versions.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+curl -L https://github.com/ferenk/nodejs_multi-version-support/releases/download/latest/client_versions.tar.bz2 -o client_versions.tar.bz2
+tar -xjf client_versions.tar.bz2
+rm client_versions.tar.bz2
